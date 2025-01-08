8e9529c70272aedad9f2b6d248bccd719b58e5ad
NafisMahmudAyon <nafismahmudayon@gmail.com>
2025-01-08 17:36:20 +0600

    Version 1.0.5

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ document.addEventListener("DOMContentLoaded", () => {
 			const accordionID = dataElement.getAttribute("data-accordion-id");
 
 			// Fetch accordion data
-			fetch(`http://accordion-editor.nafisbd.com/api/accordion/fetch-data?id=${accordionID}`)
+			fetch(
+				`http://accordion-editor.nafisbd.com/api/accordion/fetch-data?id=${accordionID}`
+			)
 				.then((response) => {
 					if (!response.ok) {
 						throw new Error(`HTTP error! Status: ${response.status}`);
@@ -28,76 +30,77 @@ document.addEventListener("DOMContentLoaded", () => {
 				})
 				.then((data) => {
 					const mainData = data.data[0];
-          const accordionData = mainData?.content
+					const accordionData = mainData?.content;
 
-          if(mainData.status === "publish"){
-// Render the Accordion component with fetched data
-            ReactDOM.render(
-              <Accordion
-                activeItem={accordionData?.global?.activeItems?.map(
-                  (index) => `item-${index + 1}`
-                )}
-                iconEnabled={accordionData?.global?.iconEnabled ?? true}
-                iconPosition={accordionData?.global?.iconPosition}
-                className={accordionData?.global?.accordionClassName}
-                multiple={accordionData?.global?.multiple ?? false}
-                reset={true}>
-                {accordionData.items.map((item, index) => (
-                  <AccordionItem
-                    key={index}
-                    id={`item-${index + 1}`}
-                    disabled={item?.disabled}>
-                    <AccordionHeader
-                      iconEnabled={item.iconEnabled}
-                      iconPosition={item.iconPosition}
-                      iconClassName={item.iconClassName}
-                      activeIconClassName={item.activeIconClassName}
-                      activeIcon={
-                        item.activeIcon ? (
-                          <IconSelector
-                            iconType={item.activeIconType}
-                            iconName={item.activeIcon}
-                          />
-                        ) : (
-                          <IconSelector
-                            iconType={accordionData?.global?.activeIconType}
-                            iconName={accordionData?.global?.activeIcon}
-                          />
-                        )
-                      }
-                      inactiveIcon={
-                        item.inactiveIcon ? (
-                          <IconSelector
-                            iconType={item.inactiveIconType}
-                            iconName={item.inactiveIcon}
-                          />
-                        ) : (
-                          <IconSelector
-                            iconType={accordionData?.global?.inactiveIconType}
-                            iconName={accordionData?.global?.inactiveIcon}
-                          />
-                        )
-                      }
-                      className={item.headerClassName}
-                      labelClassName={item.labelClassName}
-                      activeHeaderClassName={item.activeHeaderClassName}
-                      activeLabelClassName={item.activeLabelClassName}>
-                      {item.headerLabel}
-                    </AccordionHeader>
-                    <AccordionContent className={item.contentClassName}
-                    dangerouslySetInnerHTML={{ __html: item.content }} />
-                  </AccordionItem>
-                ))}
-              </Accordion>,
-              container
-            );
-          }
-					else
-            ReactDOM.render(
-						<div>Please publish your accordion first</div>,
-						container
-					);
-
+					if (mainData.status === "publish") {
+						// Render the Accordion component with fetched data
+						ReactDOM.render(
+							<Accordion
+								activeItem={accordionData?.global?.activeItems?.map(
+									(index) => `item-${index + 1}`
+								)}
+								iconEnabled={accordionData?.global?.iconEnabled ?? true}
+								iconPosition={accordionData?.global?.iconPosition}
+								className={accordionData?.global?.accordionClassName}
+								multiple={accordionData?.global?.multiple ?? false}
+								reset={true}>
+								{accordionData.items.map((item, index) => (
+									<AccordionItem
+										key={index}
+										id={`item-${index + 1}`}
+										disabled={item?.disabled}>
+										<AccordionHeader
+											iconEnabled={item.iconEnabled}
+											iconPosition={item.iconPosition}
+											iconClassName={item.iconClassName}
+											activeIconClassName={item.activeIconClassName}
+											activeIcon={
+												item.activeIcon ? (
+													<IconSelector
+														iconType={item.activeIconType}
+														iconName={item.activeIcon}
+													/>
+												) : (
+													<IconSelector
+														iconType={accordionData?.global?.activeIconType}
+														iconName={accordionData?.global?.activeIcon}
+													/>
+												)
+											}
+											inactiveIcon={
+												item.inactiveIcon ? (
+													<IconSelector
+														iconType={item.inactiveIconType}
+														iconName={item.inactiveIcon}
+													/>
+												) : (
+													<IconSelector
+														iconType={accordionData?.global?.inactiveIconType}
+														iconName={accordionData?.global?.inactiveIcon}
+													/>
+												)
+											}
+											className={item.headerClassName}
+											labelClassName={item.labelClassName}
+											activeHeaderClassName={item.activeHeaderClassName}
+											activeLabelClassName={item.activeLabelClassName}>
+											{item.headerLabel}
+										</AccordionHeader>
+										<AccordionContent
+											className={item.contentClassName}
+											dangerouslySetInnerHTML={{ __html: item.content }}
+										/>
+									</AccordionItem>
+								))}
+							</Accordion>,
+							container
+						);
+					} else {
+						ReactDOM.render(
+							<div>Please publish your accordion first</div>,
+							container
+						);
+					}
 				})
 				.catch((error) => {
 					console.error("Failed to fetch accordion data:", error);
